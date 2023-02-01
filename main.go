c737bfdc6312e11347eeba830d5ce2f73d103c83
Jiahe <yi.jiahe00@gmail.com>
2023-02-01 18:11:13 +0800

    Add EI parsing modify outlocation

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"archive/zip"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
-
-	"github.com/Yi-Jiahe/evtc"
 )
 
 type Config struct {
@@ -24,13 +22,16 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Ensure config dir exists
 	configDir := filepath.Join(homeDir, ".arcdps-log-uploader")
-	configFilePath := filepath.Join(configDir, "config.json")
 	if _, err := os.Stat(configDir); os.IsNotExist(err) {
 		os.Mkdir(configDir, 0750)
+	}
 
+	configFilePath := filepath.Join(configDir, "config.json")
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
 		config := Config{
-			LogPath:   filepath.Join(homeDir, `Documents\Guild Wars 2\addons\arcdps\arcdps.cbtlogs`),
+			LogPath:   filepath.Join(homeDir, "Documents", "Guild Wars 2", "addons", "arcdps", "arcdps.cbtlogs", "Ankka"),
 			BucketKey: "",
 		}
 
@@ -59,39 +60,54 @@ func main() {
 
 	json.Unmarshal(byteValue, &config)
 
+	EIPath := filepath.Join(homeDir, "GW2EI", "GuildWars2EliteInsights.exe")
+	EIConfigPath := filepath.Join(homeDir, ".arcdps-log-uploader", "elite_insights.conf")
+
+	tempDirPath := filepath.Join(configDir, "temp")
+	os.Mkdir(tempDirPath, 0750)
+
 	filepath.Walk(config.LogPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if strings.HasSuffix(info.Name(), ".zevtc") {
+			fmt.Printf("%v\n", path)
 			fmt.Printf("%v\n", info.Name())
 
-			// Unzip compressed evtc file
-			r, err := zip.OpenReader(path)
+			cmd := exec.Command(EIPath, "-c", EIConfigPath, "-p", path)
+			err := cmd.Run()
 			if err != nil {
 				log.Panic(err)
 			}
-			defer r.Close()
 
-			for _, f := range r.File {
-				rc, err := f.Open()
-				if err != nil {
-					log.Panic(err)
-				}
-				defer rc.Close()
+			// // Unzip compressed evtc file
+			// r, err := zip.OpenReader(path)
+			// if err != nil {
+			// 	log.Panic(err)
+			// }
+			// defer r.Close()
 
-				// Parse evtc file
-				header, _, _, err := evtc.ParseHeader(rc)
-				if err != nil {
-					log.Panic(err)
-				}
-				fmt.Printf("%v %d\n", string(header.Date[:]), header.Boss)
+			// for _, f := range r.File {
+			// 	rc, err := f.Open()
+			// 	if err != nil {
+			// 		log.Panic(err)
+			// 	}
+			// 	defer rc.Close()
 
-				// fmt.Printf("%v\n", chain)
-			}
+			// 	Parse evtc file
+			// 	header, _, _, err := evtc.ParseHeader(rc)
+			// 	if err != nil {
+			// 		log.Panic(err)
+			// 	}
+			// 	fmt.Printf("%v %d\n", string(header.Date[:]), header.Boss)
+
+			// 	// fmt.Printf("%v\n", chain)
+			// }
 		}
 
 		return nil
 	})
+
+	os.RemoveAll(tempDirPath)
 }
